1b3e091dbe187ab0413b5f27a85b4066015c8f99
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-03-28 02:04:54 -0500

    breadcrumb update

--- neurological/multiple-sclerosis-diagnosis/index.php
+++ neurological/multiple-sclerosis-diagnosis/index.php
@@ -5,7 +5,7 @@
   <li><a href="/">Home</a></li>
   <li><a href="/categories/">Categories</a></li>
   <li><a href="/neurological/">Neurology</a></li>
-  <li class="active">Here</li>
+  <li class="active">Multiple Sclerosis Diagnosis</li>
 </ol>
 
 <div id="container" class="container">
