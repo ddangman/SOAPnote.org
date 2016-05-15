9dacf9ee7ab139a38bc41a6f47d5f06854e8a979
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-05-15 03:26:45 -0500

    simplifying pages
    no significant content changes, just trying to make front-facing pages
    simpler and more compelling

--- help/index.php
+++ help/index.php
@@ -3,19 +3,17 @@
 ?>
 <ul class="breadcrumb">
     <li><a href="/">Home</a></li>
-    <li class="active">Help</li>
+    <li class="active">Ask</li>
 </ul>
 
 <div id="container" class="container">
-If you'd like to have a SOAPnote template or calculator made for you or your organization, please visit my <a href="https://www.indiegogo.com/projects/clinical-calculators-and-note-templates/x/46196#/">IndieGoGo campaign</a>.
-
-Please feel free to tweet or <a href="mailto:mark@soapnote.org">email me</a> if you have any questions or you would like to report a problem.
+<iframe src="https://docs.google.com/forms/d/14quI6peXcHGJjcy5vGArIZ40ssskYNfBstvUHYFBfOk/viewform?embedded=true" width="760" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </br>
 </br>
 <a class="twitter-timeline" href="https://twitter.com/SOAPNOTEorg" data-widget-id="420884108961538048">Tweets by @SOAPNOTEorg</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </br>
 </br>
-<em>Last Updated May 14, 2016</em>
+<em>Last Updated May 15, 2016</em>
 </div>
 <?php 
  include('../lib/main-footer.php');
