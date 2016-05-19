dfb425eaef307f89c55d27f8a012b4e999fbd2c7
Mark Morgan <mark@soapnote.org>
2016-05-18 23:28:58 -0700

    updated the git wiki

--- lib/main-footer.php
+++ lib/main-footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class="container-fluid">
-		<p class="text-muted">SOAPnote.org is an <a href="https://github.com/soapnote/SOAPnote-site" target="_blank">Open Source Project</a></p>
+		<p class="text-muted">SOAPnote.org is an <a href="https://github.com/soapnote/SOAPnote-site/wiki" target="_blank">Open Source Project</a></p>
 	</div>
 </footer>
 </body>
