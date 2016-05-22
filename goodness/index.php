dd7e480ea692baccbcb0b3d0b9e7f8a603200972
Mark Morgan <mark@soapnote.org>
2016-05-21 23:01:04 -0700

    added badges for iOS and Android

--- goodness/index.php
+++ goodness/index.php
@@ -8,7 +8,14 @@
 
 <div id="container" class="container">
 <h2>Free iOS and Android App</h2>
-There's a SOAPnote app that includes curated content from the website.  If you don't have good cellular or wifi where you work (like me) this might come in handy.  It's at <a href="https://play.google.com/store/apps/details?id=org.soapnote.app" target="_blank">Google Play</a> for Android.  It is at <a href="https://itunes.apple.com/us/app/soapnote.org-app/id1046761652?mt=8" target="_blank">iTunes</a> for Apple devices.
+There's a SOAPnote app that includes curated content from the website.  If you don't have good cellular or wifi where you work (like me) this might come in handy.  Choose the appropriate platform below:
+</br>
+<a href="https://geo.itunes.apple.com/us/app/soapnote.org-app/id1046761652?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/images/badges/en-us/badge_appstore-lrg.svg) no-repeat;width:165px;height:40px;"></a>
+</br>
+<a href="https://play.google.com/store/apps/details?id=org.soapnote.app">
+  <img alt="Get it on Google Play"
+       src="https://developer.android.com/images/brand/en_generic_rgb_wo_45.png" />
+</a>
 </br>
 </br>
 Please go to the <a href="http://www.soapnote.org/help/">help page</a> if you have a question, an idea, or if you would like to report a problem.
@@ -17,7 +24,7 @@ Please go to the <a href="http://www.soapnote.org/help/">help page</a> if you ha
 And if you like this site and/or the app feel free to donate <a href="http://paypal.me/soapnote/2">a cup of coffee</a> to keep me working on it.
 </br>
 </br>
-<em>Last Updated May 15, 2016</em>
+<em>Last Updated May 22, 2016</em>
 </div>
 <?php 
  include('../lib/main-footer.php');
